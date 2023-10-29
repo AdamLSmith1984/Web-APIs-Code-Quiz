0445031bab572d455f89d68c972c958173e94020
Adam Smith <asmith5144@hotmail.com>
2023-10-29 13:55:40 -0400

    added questions.js

---
+++ JS/question.js
@@ -0,0 +1,35 @@
+
+
+// list of all questions, choices, and answers
+var questions = [
+    {
+        title: "Which one is a looping structure in JavaScript?",
+        choices: ["All the below", "For", "While", "do-while loops"],
+        answer: "All the below"
+    },
+    {
+        title: "What is the type of Pop up boxes available in JavaScript?:",
+        choices: ["Alert", "Confirm", "Prompt", "All the above"],
+        answer: "All the above"
+    },
+    {
+        title: "What is the data type of variables in JavaScript",
+        choices: ["Object data types", "Function data type", "None of the above", "All of the above"],
+        answer: "Object data types"
+    },
+    {
+        title: "What are the two basic groups of the data types in JavaScript",
+        choices: ["Primitive and attribute", "Primitive and reference types", "Reference types and attribute", "None of the above"],
+        answer: "Primitive and reference types"
+    },
+    {
+        title: "A very useful tool used during develpoment and debugging for prinintg content to the debugger is:",
+        choices: ["Javacript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    {
+        title: "Boolean operators that can be used in JavaScript include:",
+        choices: ["'And' Operator &&", "'Or' Operator ||", "'Not' Operator !", "All the above"],
+    },
+
+];
